e06dd934d736f3aa625b55edb52d16ef2677173c
tuxuser <tuxuser360@gmail.com>
2018-03-21 04:55:05 +0100

    Add token refresh test

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,6 +1,7 @@
 import pytest
 from betamax import Betamax
 
+from xbox.webapi.authentication.token import RefreshToken
 from xbox.webapi.common.exceptions import AuthenticationException
 from xbox.webapi.authentication.token import AccessToken, RefreshToken, UserToken, XSTSToken
 
@@ -28,6 +29,26 @@ def test_auth_valid_credentials(auth_manager):
 
     with Betamax(auth_manager.session).use_cassette('full_auth'):
         auth_manager.authenticate(do_refresh=False)
+
+    assert auth_manager.xsts_token.is_valid is True
+    assert auth_manager.access_token.is_valid is True
+    assert auth_manager.refresh_token.is_valid is True
+    assert auth_manager.user_token.is_valid is True
+    assert auth_manager.userinfo.userhash == '1674471606081042789'
+    assert auth_manager.userinfo.xuid == '2535428504476914'
+    assert auth_manager.userinfo.gamertag == 'xboxWebapiGamertag'
+
+
+def test_auth_refresh_token(auth_manager):
+    auth_manager.refresh_token = RefreshToken(
+        "CuZ*4TX7!SAF33cW*kzdFmgCLPRcz0DtUHFqjQgF726!FG3ScC5yMiDLsJYJ03m4fURrzf3J7X8l6A8mJGhHoRf42aHeJLrtp6wS"
+        "Jh*PudaQdPNGJZHD1CpU4diJJxz0zhrijFsaAYXMqf3mSU7EerR5RtdHOwbcrlRlj7TBQ9RdLqWpy9KWsNhyPrwOMDJnBfAf3xsZ"
+        "3g3QkmMeKGil85*q*MV*YqMPZTa8UVLPfM!jJeHwOjVhWPaYVq4hf6zIAwSLJl1Reo6GbkkPktrK3laFBGeqSkq651YgdjwtepwC"
+        "Ef7oMwzz8c8msv8l95RU*QmtIjdRFd!fYtQctiGLDGs$"
+    )
+
+    with Betamax(auth_manager.session).use_cassette('token_refresh'):
+        auth_manager.authenticate(do_refresh=True)
 
     assert auth_manager.xsts_token.is_valid is True
     assert auth_manager.access_token.is_valid is True
