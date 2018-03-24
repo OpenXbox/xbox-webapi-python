0b411340371dfc3b2cdc1a6f7c777fdd96bf09ee
tuxuser <tuxuser360@gmail.com>
2018-03-24 17:20:06 +0100

    XboxLiveClient tests

---
+++ tests/test_xbl_client.py
@@ -0,0 +1,25 @@
+import pytest
+from xbox.webapi.api.client import XboxLiveClient
+
+
+def test_xuid_from_int():
+    client = XboxLiveClient('userhash', 'token', 1234567890)
+
+    assert client.xuid == 1234567890
+
+
+def test_xuid_from_string():
+    client = XboxLiveClient('userhash', 'token', '1234567890')
+
+    assert client.xuid == 1234567890
+
+
+def test_invalid_xuid_format():
+    with pytest.raises(ValueError):
+        client = XboxLiveClient('userhash', 'token', b'1234567890')
+
+
+def test_authorization_header():
+    client = XboxLiveClient('userhash', 'token', '1234567890')
+
+    assert client.session.headers['Authorization'] == 'XBL3.0 x=userhash;token'
