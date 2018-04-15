ecf9a5517a62b0181598c6e4e2418a624b894fb0
Schamper <Schamper@users.noreply.github.com>
2018-04-15 12:03:53 +0200

    Cleaner setup.py

--- setup.py
+++ setup.py
@@ -4,47 +4,12 @@
 from setuptools import setup
 
 
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
-
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
-requirements = [
-    'requests',
-    'python-dateutil',
-    'demjson',
-    'urwid'
-]
-
-setup_requirements = ['pytest-runner']
-
-test_requirements = [
-    'pytest',
-    'betamax'
-]
-
-dev_requirements = [
-    'pip==9.0.1',
-    'bumpversion==0.5.3',
-    'wheel==0.30.0',
-    'watchdog==0.8.3',
-    'flake8==3.5.0',
-    'tox==2.9.1',
-    'coverage==4.5.1',
-    'Sphinx==1.7.1',
-    'twine==1.10.0',
-    'betamax==0.8.1',
-    'pytest==3.4.2',
-    'pytest-runner==2.11.1'
-]
-
 setup(
     name="xbox-webapi",
     version="1.0.9",
     author="OpenXbox",
     description="A library to authenticate with Windows Live/Xbox Live and use their API",
-    long_description=readme + '\n\n' + history,
+    long_description=open('README.rst').read() + '\n\n' + open('HISTORY.rst').read(),
     license="GPL",
     keywords="xbox one live api",
     url="https://github.com/OpenXbox/xbox-webapi-python",
@@ -68,11 +33,33 @@ setup(
         "Programming Language :: Python :: 3.6"
     ],
     test_suite="tests",
-    install_requires=requirements,
-    setup_requires=setup_requirements,
-    tests_require=test_requirements,
+    install_requires=[
+        'requests',
+        'python-dateutil',
+        'demjson',
+        'appdirs',
+        'urwid'
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=[
+        'pytest',
+        'betamax'
+    ],
     extras_require={
-        'dev': dev_requirements,
+        'dev': [
+            'pip==9.0.1',
+            'bumpversion==0.5.3',
+            'wheel==0.30.0',
+            'watchdog==0.8.3',
+            'flake8==3.5.0',
+            'tox==2.9.1',
+            'coverage==4.5.1',
+            'Sphinx==1.7.1',
+            'twine==1.10.0',
+            'betamax==0.8.1',
+            'pytest==3.4.2',
+            'pytest-runner==2.11.1'
+        ],
     },
     entry_points={
         'console_scripts': [
