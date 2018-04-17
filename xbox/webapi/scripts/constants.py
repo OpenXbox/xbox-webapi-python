98d203c648a56f64a4dfc2e3531ee8e4472bda21
tuxuser <tuxuser360@gmail.com>
2018-04-17 11:38:17 +0200

    constants: Implictly create user dir

--- xbox/webapi/scripts/constants.py
+++ xbox/webapi/scripts/constants.py
@@ -4,3 +4,5 @@ from appdirs import user_data_dir
 
 DATA_DIR = user_data_dir('xbox', 'OpenXbox')
 TOKENS_FILE = os.path.join(DATA_DIR, 'tokens.json')
+if not os.path.exists(DATA_DIR):
+    os.mkdir(DATA_DIR)
