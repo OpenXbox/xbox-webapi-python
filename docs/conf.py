2e2b9a70ad4df020db5898ce6249b82441b7c738
tuxuser <tuxuser360@gmail.com>
2018-04-17 14:47:02 +0200

    Bump version: 1.0.9 → 1.1.0

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ author = 'OpenXbox'
 # The short X.Y version
 version = '1.0'
 # The full version, including alpha/beta/rc tags
-release = '1.0.9'
+release = '1.1.0'
 
 
 # -- General configuration ---------------------------------------------------
