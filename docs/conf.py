22875c2a592bbdb9dca57cfa84f4a1bbb2287943
tuxuser <tuxuser360@gmail.com>
2018-05-06 21:08:31 +0200

    Bump version: 1.1.1 → 1.1.2

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ author = 'OpenXbox'
 # The short X.Y version
 version = '1.0'
 # The full version, including alpha/beta/rc tags
-release = '1.1.1'
+release = '1.1.2'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name="xbox-webapi",
-    version="1.1.1",
+    version="1.1.2",
     author="OpenXbox",
     description="A library to authenticate with Windows Live/Xbox Live and use their API",
     long_description=open('README.rst').read() + '\n\n' + open('HISTORY.rst').read(),
