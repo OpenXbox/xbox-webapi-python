281d37acb6c9b2e7033a122b5f199f05c004ba0b
dominikfehr <dominik@fe.hr>
2020-08-06 12:06:31 +0200

    fixed messaging

--- xbox/webapi/api/provider/message.py
+++ xbox/webapi/api/provider/message.py
@@ -1,14 +1,16 @@
 """
 Message - Read and send messages
 """
+from pathlib import Path
 from xbox.webapi.api.provider.baseprovider import BaseProvider
 
 
 class MessageProvider(BaseProvider):
-    MSG_URL = "https://msg.xboxlive.com"
+    MSG_URL = "https://xblmessaging.xboxlive.com"
     HEADERS_MESSAGE = {'x-xbl-contract-version': '1'}
 
-    def get_message_inbox(self, skip_items=0, max_items=100):
+
+    def get_message_inbox(self):
         """
         Get messages
 
@@ -19,73 +21,102 @@ class MessageProvider(BaseProvider):
         Returns:
             :class:`requests.Response`: HTTP Response
         """
-        url = self.MSG_URL + "/users/xuid(%s)/inbox" % self.client.xuid
-        params = {
-            'skipItems': skip_items,
-            'maxItems': max_items
-        }
+        url = self.MSG_URL + "/network/xbox/users/xuid(%s)/inbox" % self.client.xuid
         return self.client.session.get(url, params=params, headers=self.HEADERS_MESSAGE)
 
-    def get_message(self, message_id):
+
+    def get_conversation(self, xuid):
         """
         Get detailed message info
 
         Args:
-            message_id (str): Message Id
+            xuid (str): XUID of chat participant
 
         Returns:
             :class:`requests.Response`: HTTP Response
         """
-        url = self.MSG_URL + "/users/xuid(%s)/inbox/%s" % (self.client.xuid, message_id)
+        if not isinstance(xuid, str):
+            raise TypeError('Expecting XUID string')
+
+        url = self.MSG_URL + "/network/xbox/users/xuid(%s)/conversations/users/xuid(%s)?maxItems=100" % (self.client.xuid, xuid)
         return self.client.session.get(url, headers=self.HEADERS_MESSAGE)
 
-    def delete_message(self, message_id):
-        """
-        Delete message
 
-        **NOTE**: Returns HTTP Status Code **204** on success
-
-        Args:
-            message_id (str): Message Id
-
-        Returns:
-            :class:`requests.Response`: HTTP Response
-        """
-        url = self.MSG_URL + "/users/xuid(%s)/inbox/%s" % (self.client.xuid, message_id)
-        return self.client.session.delete(url, headers=self.HEADERS_MESSAGE)
-
-    def send_message(self, message_text, gamertags=None, xuids=None):
+    def send_message(self, message_text, xuid):
         """
         Send message to a list of gamertags
 
-        Only one of each recipient types can be supplied,
-        either gamertags **or** xuids
-
         Args:
-            gamertags (list): List of gamertags
+            xuid: id of recipient
 
         Returns:
             :class:`requests.Response`: HTTP Response
         """
+        if not isinstance(xuid, str):
+            raise TypeError('Expecting XUID string')
+
         if not isinstance(message_text, str):
             raise TypeError('Expecting message_text string')
         elif len(message_text) > 256:
             raise ValueError('Message text exceeds max length of 256 chars')
 
-        if gamertags and xuids:
-            raise ValueError('Either pass gamertags or xuids, not both!')
-        elif gamertags:
-            recipients = [dict(gamertag=gtg) for gtg in gamertags]
-        elif xuids:
-            recipients = [dict(xuid=xid) for xid in xuids]
-        else:
-            raise ValueError('No recipients are passed')
-
-        url = self.MSG_URL + "/users/xuid(%s)/outbox" % self.client.xuid
+        url = self.MSG_URL + "/network/xbox/users/xuid(%s)/conversations/users/xuid(%s)" % (self.client.xuid, xuid)
         post_data = {
-            'header': {
-                'recipients': recipients
-            },
-            'messageText': message_text
-        }
+            'parts': [
+             {
+                'text': message_text,
+                'contentType':'text',
+                'version':0
+             }]
+	}
+        return self.client.session.post(url, json=post_data, headers=self.HEADERS_MESSAGE)
+
+
+    def send_image(self, image_url, xuid, media_type="unknown", message_text="unknown"):
+        """
+        Send message to a list of gamertags
+
+        Args:
+            image_url: url to image file
+            media_type: extension/filetype of provided image
+            xuid: id of recipient
+
+        Returns:
+            :class:`requests.Response`: HTTP Response
+        """
+        if not isinstance(xuid, str):
+            raise TypeError('Expecting XUID string')
+
+        if not isinstance(message_text, str):
+            raise TypeError('Expecting message_text string')
+        elif len(message_text) > 256:
+            raise ValueError('Message text exceeds max length of 256 chars')
+        elif message_text == "unknown":
+             message_text = image_url
+
+        if not isinstance(image_url, str):
+            raise TypeError('Expecting image_url string')
+        elif len(image_url) > 256:
+            raise ValueError('Image URL exceeds max length of 256 chars')
+
+        if not isinstance(media_type, str):
+            raise TypeError('Expecting image_url string')
+        elif len(media_type) > 256:
+            raise ValueError('Image URL exceeds max length of 256 chars')
+        elif media_type == "unknown":
+             media_type = Path(image_url).suffix
+
+
+        url = self.MSG_URL + "/network/xbox/users/xuid(%s)/conversations/users/xuid(%s)" % (self.client.xuid, xuid)
+        post_data = {
+            'parts': [
+             {
+                'text': message_text,
+                'mediaUri': image_url,
+                'shouldObscure':False,
+                'contentType':'weblinkMedia',
+                'mediaType':media_type,
+                'version':0
+             }]
+	}
         return self.client.session.post(url, json=post_data, headers=self.HEADERS_MESSAGE)
