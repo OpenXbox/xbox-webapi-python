402ca1134a68efd9ecfcff6e805a0e1d1a44d01b
Jason Hunter <hunterjm@gmail.com>
2020-10-09 11:12:21 -0400

    update titlehub models

--- tests/test_titlehub.py
+++ tests/test_titlehub.py
@@ -11,7 +11,7 @@ async def test_titlehub_titlehistory(aresponses, xbl_client):
     ret = await xbl_client.titlehub.get_title_history(987654321)
     await xbl_client._auth_mgr.session.close()
 
-    assert len(ret.titles) == 5
+    assert len(ret.titles) == 144
 
     aresponses.assert_plan_strictly_followed()
 
--- xbox/webapi/api/provider/titlehub/models.py
+++ xbox/webapi/api/provider/titlehub/models.py
@@ -1,14 +1,18 @@
+from datetime import datetime
+from enum import Enum
 from typing import Any, List, Optional
 
-from xbox.webapi.common.models import CamelCaseModel
+from xbox.webapi.common.models import CamelCaseModel, PascalCaseModel
 
 
-class TitleFields:
+class TitleFields(str, Enum):
+    SERVICE_CONFIG_ID = "scid"
     ACHIEVEMENT = "achievement"
+    STATS = "stats"
+    GAME_PASS = "gamepass"
     IMAGE = "image"
-    FRIENDS_WHO_PLAYED = "friendswhoplayed"
-    SERVICE_CONFIG_ID = "SCID"
     DETAIL = "detail"
+    FRIENDS_WHO_PLAYED = "friendswhoplayed"
     ALTERNATE_TITLE_ID = "alternateTitleId"
 
 
@@ -21,45 +25,79 @@ class Achievement(CamelCaseModel):
     source_version: int
 
 
+class Stats(CamelCaseModel):
+    source_version: int
+
+
+class GamePass(CamelCaseModel):
+    is_game_pass: bool
+
+
 class Image(CamelCaseModel):
     url: str
     type: str
 
 
+class TitleHistory(CamelCaseModel):
+    last_time_played: datetime
+    visible: bool
+    can_hide: bool
+
+
+class Attribute(CamelCaseModel):
+    applicable_platforms: Optional[List[str]]
+    maximum: Optional[int]
+    minimum: Optional[int]
+    name: str
+
+
+class Availability(PascalCaseModel):
+    actions: List[str]
+    availability_id: str
+    platforms: List[str]
+    sku_id: str
+
+
 class Detail(CamelCaseModel):
+    attributes: List[Attribute]
+    availabilities: List[Availability]
+    capabilities: List[str]
     description: str
-    short_description: Optional[str]
-    publisher_name: str
     developer_name: str
-    vui_display_name: Any
-    release_date: str
+    publisher_name: str
     min_age: int
+    release_date: Optional[datetime]
+    short_description: Optional[str]
+    vui_display_name: Optional[str]
+    xbox_live_gold_required: bool
 
 
 class Title(CamelCaseModel):
     title_id: str
     pfn: Optional[str]
-    bing_id: Any
-    service_config_id: str
+    bing_id: Optional[str]
+    service_config_id: Optional[str]
     windows_phone_product_id: Optional[str]
     name: str
     type: str
     devices: List[str]
     display_image: str
     media_item_type: str
-    modern_title_id: str
+    modern_title_id: Optional[str]
     is_bundle: bool
-    achievement: Achievement
-    stats: Any
-    images: List[Image]
-    title_history: Any
+    achievement: Optional[Achievement]
+    stats: Optional[Stats]
+    game_pass: Optional[GamePass]
+    images: Optional[List[Image]]
+    title_history: Optional[TitleHistory]
     detail: Optional[Detail]
     friends_who_played: Any
     alternate_title_ids: Any
     content_boards: Any
-    xbox_live_tier: str
+    xbox_live_tier: Optional[str]
+    is_streamable: Optional[bool]
 
 
 class TitlehubResponse(CamelCaseModel):
-    xuid: Any
+    xuid: Optional[str]
     titles: List[Title]
