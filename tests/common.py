8ca0790de8f6783b40d2b61cf9bdf70e22255aed
nereg <okv23200@gmail.com>
2021-03-29 13:54:28 +0300

    Now ./tests/common.py reads files as UTF-8

--- tests/common.py
+++ tests/common.py
@@ -5,6 +5,6 @@ TEST_PATH = os.path.dirname(__file__)
 
 def get_response(name):
     """Read a response file."""
-    with open(f"{TEST_PATH}/data/responses/{name}.json") as f:
+    with open(f"{TEST_PATH}/data/responses/{name}.json",encoding='utf8') as f:
         response = f.read()
     return response
