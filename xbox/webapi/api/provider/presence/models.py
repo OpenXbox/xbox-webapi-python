f2cf3da8a36b4e154baefc3aaaf59351f113bdec
Gurrman375 <james.gurr375@gmail.com>
2021-05-07 14:52:35 -0700

    Updated Presence Api
    Now the provider will print the user the Device Record in a clean print.
    
    Example:
    ```
    devices=[DeviceRecord(titles=[TitleRecord(id='750323071', name='', activity=None, lastModified='2021-05-07T21:50:47.7329203Z', placement='Background', state='Active'), TitleRecord(id='342226876', name='', activity=None, lastModified='2021-05-07T21:50:47.7329203Z', placement='Full', state='Active')], type='Scarlett'), DeviceRecord(titles=[TitleRecord(id='1144039928', name='', activity=None, lastModified='2021-05-07T21:46:51.484922Z', placement='Full', state='Active')], type='Win32'), DeviceRecord(titles=[TitleRecord(id='1144039928', name='', activity=None, lastModified='2021-05-07T21:48:28.4841154Z', placement='Full', state='Active')], type='WindowsOneCore')]
    ```

--- xbox/webapi/api/provider/presence/models.py
+++ xbox/webapi/api/provider/presence/models.py
@@ -1,8 +1,6 @@
 from enum import Enum
 from typing import List, Optional
-
 from xbox.webapi.common.models import CamelCaseModel
-
 
 class PresenceLevel(str, Enum):
     USER = "user"
@@ -16,13 +14,30 @@ class LastSeen(CamelCaseModel):
     title_id: str
     title_name: str
     timestamp: str
+    
+class ActivityRecord(CamelCaseModel):
+    richPresence: str
+    media: str
+    
+class TitleRecord(CamelCaseModel):
+    id: str
+    name: str
+    activity: Optional[List[ActivityRecord]]
+    lastModified: str
+    placement: str
+    state: str
 
+class DeviceRecord(CamelCaseModel):
+    titles: List[TitleRecord]
+    type: str
+        
 
 class PresenceItem(CamelCaseModel):
     xuid: str
     state: str
     last_seen: Optional[LastSeen]
-
+    devices: Optional[List[DeviceRecord]]
+    
 
 class PresenceBatchResponse(CamelCaseModel):
     __root__: List[PresenceItem]
