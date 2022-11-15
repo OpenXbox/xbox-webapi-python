2c2633e33b95df075ea6c44b14e41f86d933abbf
James Cahill <jcahill773@gmail.com>
2022-11-15 12:08:02 +0000

    Test parsing of RATE_LIMITS in different formats

--- tests/test_ratelimits.py
+++ tests/test_ratelimits.py
@@ -3,8 +3,72 @@ from httpx import Response
 import pytest
 
 from tests.common import get_response_json
+from xbox.webapi.api.provider.ratelimitedprovider import RateLimitedProvider
 
 from xbox.webapi.common.exceptions import RateLimitExceededException
+from xbox.webapi.common.ratelimits import CombinedRateLimit
+from xbox.webapi.common.ratelimits.models import TimePeriod
+
+
+def helper_test_combinedratelimit(
+    crl: CombinedRateLimit, burstLimit: int, sustainLimit: int
+):
+    burst = crl.get_limits_by_period(TimePeriod.BURST)
+    sustain = crl.get_limits_by_period(TimePeriod.SUSTAIN)
+
+    # These functions should return a list with one element
+    assert type(burst) == list
+    assert type(sustain) == list
+
+    assert len(burst) == 1
+    assert len(sustain) == 1
+
+    # Check that their limits are what we expect
+    assert burst[0].get_limit() == burstLimit
+    assert sustain[0].get_limit() == sustainLimit
+
+
+def test_ratelimitedprovider_rate_limits_same_rw_values(xbl_client):
+    class child_class(RateLimitedProvider):
+        RATE_LIMITS = {"burst": 1, "sustain": 2}
+
+    instance = child_class(xbl_client)
+
+    helper_test_combinedratelimit(instance.rate_limit_read, 1, 2)
+    helper_test_combinedratelimit(instance.rate_limit_write, 1, 2)
+
+
+def test_ratelimitedprovider_rate_limits_diff_rw_values(xbl_client):
+    class child_class(RateLimitedProvider):
+        RATE_LIMITS = {
+            "burst": {"read": 1, "write": 2},
+            "sustain": {"read": 3, "write": 4},
+        }
+
+    instance = child_class(xbl_client)
+
+    helper_test_combinedratelimit(instance.rate_limit_read, 1, 3)
+    helper_test_combinedratelimit(instance.rate_limit_write, 2, 4)
+
+
+def test_ratelimitedprovider_rate_limits_mixed(xbl_client):
+    class burst_diff(RateLimitedProvider):
+        RATE_LIMITS = {"burst": {"read": 1, "write": 2}, "sustain": 3}
+
+    burst_diff_inst = burst_diff(xbl_client)
+
+    # Sustain values are the same (third paramater)
+    helper_test_combinedratelimit(burst_diff_inst.rate_limit_read, 1, 3)
+    helper_test_combinedratelimit(burst_diff_inst.rate_limit_write, 2, 3)
+
+    class sustain_diff(RateLimitedProvider):
+        RATE_LIMITS = {"burst": 4, "sustain": {"read": 5, "write": 6}}
+
+    sustain_diff_inst = sustain_diff(xbl_client)
+
+    # Burst values are the same (second paramater)
+    helper_test_combinedratelimit(sustain_diff_inst.rate_limit_read, 4, 5)
+    helper_test_combinedratelimit(sustain_diff_inst.rate_limit_write, 4, 6)
 
 
 @pytest.mark.asyncio
